cb34ac1041aa737f3f24640ba012d742801fd281
manuel <lreyes03@mail.com>
2021-09-30 14:31:24 +0000

    print the string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
 
-
-int main ()
+int main(void)
 {
-    char s[]= "\"Programming is like building a multilingual puzzle \n";
-
-    puts(s);
-    return (0);
+char s[] = "\"Programming is like building a multilingual puzzle";
+puts(s);
+return (0);
 }
-
-
