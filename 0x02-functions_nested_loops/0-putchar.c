c4bfbd1db2c110df90228c9e22d30ce44ea20e8c
manuel <lreyes03@mail.com>
2021-10-05 23:14:59 +0000

    init commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - print _putchar
+ * Description - print string _putchar
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
