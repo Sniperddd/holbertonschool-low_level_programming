f74bde01b19800dbabfc10432d9012375d040951
manuel <lreyes03@mail.com>
2021-10-06 03:48:58 +0000

    two intenger return result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - sum two number
+ * @n1: arg onw
+ * @n2: arg two
+ * Return: sum
+ */
+
+int add(int n1, int n2)
+{
+	int sum;
+
+	sum = n1 + n2;
+	return (sum);
+}
