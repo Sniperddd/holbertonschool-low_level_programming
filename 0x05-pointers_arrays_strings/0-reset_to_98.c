6597b4a8cf83e7813d5ff69487103135dec1abd6
manuel <lreyes03@mail.com>
2021-10-11 17:15:13 +0000

    init commmit Pointer project

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - updates the value
+ * Description - update the value it point
+ * @n: pointer input --> update
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
