0608c7302b45121faf9cd7542247c7b0fef1065f
manuel <lreyes03@mail.com>
2021-10-11 21:54:18 +0000

    prints string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev - prints a the string
+ * @s: pointer character
+ *
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int length;
+
+	for (length = 0; s[length] != '\0'; length++)
+	{
+
+	}
+	for (i = length - 1; i >= 0; i--)
+	{
+
+		_putchar(s[i]);
+
+	}
+	_putchar('\n');
+}
