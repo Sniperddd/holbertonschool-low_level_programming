1da22c0fb059e2d4d1efdb48ca930ee7a6830545
manuel <lreyes03@mail.com>
2021-10-15 02:46:52 +0000

    reverse content

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * reverse_array - reverse content
+ *@a: pointer array
+ *@n: interger value
+ */
+void reverse_array(int *a, int n)
+{
+	int b;
+	int c;
+	int temp;
+
+	c = n - 1;
+
+	while (b < c)
+	{
+		temp = a[b];
+		a[b] = a[c];
+		a[c] = temp;
+		b++;
+		c--;
+	}
+}
