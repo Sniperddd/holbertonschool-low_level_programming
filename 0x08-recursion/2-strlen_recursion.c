7e11bdfd8b0699cf504ec6ac84c4d4f3f3a1b2c1
manuel <lreyes03@mail.com>
2021-10-21 21:57:33 +0000

    length string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_strlen_recursion - length of the string
+ *@s: pointer character
+ *
+ * Return: return the length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int len;
+
+	if (*s)
+	{
+		len = _strlen_recursion(s + 1) + 1;
+	}
+	return (len);
+}
