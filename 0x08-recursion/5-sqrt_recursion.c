4ec0c6d20429b90c26a2b53fbd7913bc03174488
manuel <lreyes03@mail.com>
2021-10-22 03:57:35 +0000

    update fix sqrt root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _root - Returns the--> square root of a number
+ * @base: First input.
+ * @root: Second input.
+ * Description: Returns--> the square root.
+ * Return: Square root
+ */
+
+int _root(int base, int root)
+{
+	if (base == root * root)
+	{
+		return (root);
+	}
+	else if (base < root * root)
+	{
+		return (-1);
+	}
+	return (_root(base, root + 1));
+}
+/**
+ * _sqrt_recursion - Print my number--> for sqrt.
+ * @n: for search her sqrt.
+ * Return: The number.
+ */
+
+int _sqrt_recursion(int n)
+{
+
+	return (_root(n, 1));
+}
