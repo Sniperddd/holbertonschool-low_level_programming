4cf02361576da5bc96a0bd435ab1028fdb30f675
manuel <lreyes03@mail.com>
2021-10-27 14:56:55 +0000

    update documentation parameter

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,8 +2,11 @@
 #include <stdlib.h>
 /**
  *main - Entry point for program
- *@argc - count of args--> present cmd, running in source
- *@argv - array of char * -->pointing to args
+ *
+ *@argc:  count of args--> present cmd, running in source
+ *@argv:  array of char * -->pointing to args
+ *
+ *Return: Always 0
  */
 
 int main(int argc, char *argv[])
