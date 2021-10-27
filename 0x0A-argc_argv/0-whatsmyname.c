59ac1eedbc335cab129d4396e867669c630a799c
manuel <lreyes03@mail.com>
2021-10-27 14:52:33 +0000

    init commit print name argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - Entry point for program
+ *@argc - count of args--> present cmd, running in source
+ *@argv - array of char * -->pointing to args
+ */
+
+int main(int argc, char *argv[])
+{
+
+	if (argc < 1)
+
+		return (-1);
+	printf("%s\n", argv[0]);
+	return (0);
+}
